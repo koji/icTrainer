f826196beca919831b69cbd8674a58e5f54f1ffc
koji <kk2796@nyu.edu>
2019-05-11 21:26:07 -0400

    change file name

--- ictrainer/setup.py
+++ ictrainer/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name="ictrainer",
       version="0.2.1",
       description="cli tool for training your own image classifier with one line command!",
-      long_description=open('readme.md').read(),
+      long_description=open('Readme.md').read(),
       long_description_content_type="text/markdown",
       url="https://github.com/koji/ictrainer",
       author='koji kanao',
